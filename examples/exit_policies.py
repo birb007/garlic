48d2516362b1099099db7b74ba30bae87e2f1a36
birb007 <25516647+birb007@users.noreply.github.com>
2020-06-30 11:53:06 +0100

    include exit address in exit policy example

--- examples/exit_policies.py
+++ examples/exit_policies.py
@@ -6,12 +6,14 @@ async def main() -> None:
     client = garlic.Client(enable_cache=False)
     response = await client.get_details(
         flag=garlic.Flag.EXIT,
-        fields=("fingerprint", "exit_policy_summary", "exit_policy_v6_summary"),
+        fields=("fingerprint", "exit_addresses", "exit_policy_summary", "exit_policy_v6_summary"),
         limit=10,
     )
 
-    print("Fingerprint\t\t\t\t\tExit Policy (IPv4)\tExit Policy (IPv6)\tHTTP Exits")
+    print("Fingerprint\t\t\t\t\tExit Addresses\tExit Policy (IPv4)\tExit Policy (IPv6)\tHTTP Exits")
     for relay in response.relays:
+        exit_addr = ",".join(relay.exit_addresses) if relay.exit_addresses else "None\t"
+
         v4_policy = "Allows Exits" if relay.exit_policy_summary else "Rejects All"
         v6_policy = "Allows Exits" if relay.exit_policy_v6_summary else "Rejects All"
 
@@ -20,8 +22,8 @@ async def main() -> None:
             has_http = 80 in relay.exit_policy_v6_summary.accept_policy
 
         print(
-            f"{relay.fingerprint}\t{v4_policy}\t\t{v6_policy}"
-            f"\t\t{['No','Yes'][has_http]}"
+            f"{relay.fingerprint}\t{exit_addr}\t{v4_policy}"
+            f"\t\t{v6_policy}\t\t{['No','Yes'][has_http]}"
         )
 
 
